0e7ce07edf2cc00ec4f6653a9509531590c2bcf1
hhxiong <hhxiong@uwm.edu>
2024-07-09 22:15:08 -0500

    Updated some exceptions with the "<" operator.  Then I added some more conditions to the switch statement in my checkFlag function for that.

--- .idea/HW5/grep.c
+++ .idea/HW5/grep.c
@@ -10,8 +10,9 @@ void grepLite(int argc, char *argv[]) {
     int isCaseInsensitive = 0;
     int isLineNumber = 0;
     int isMatched = 0;
+    int isWriteToFile = 0;
 
-    checkTerminalInputs(argc, argv, &isCaseInsensitive, &isLineNumber);
+    checkTerminalInputs(argc, argv, &isCaseInsensitive, &isLineNumber, &isWriteToFile);
 
     // open and read the file
     const char *filename = argv[2];
@@ -72,18 +73,14 @@ void grepLite(int argc, char *argv[]) {
         lineNumber++;
     }
 
-    if (isMatched == 0) {
-        messageNoPatternFound();
-    }
-
-    printf("%s", stringBuilder);
-
     fclose(fptr);
+
+    messageToChoice(isWriteToFile, stringBuilder, argv, isMatched);
 }
 
-void checkTerminalInputs(int argc, char *argv[], int *isCaseInsensitive, int *isLineNumber) {
-    // Is the amount of arguments from terminal valid? At least is 3 and at most is 5.
-    if (argc < 3 || argc > 5) {
+void checkTerminalInputs(int argc, char *argv[], int *isCaseInsensitive, int *isLineNumber, int *isWriteToFile) {
+    // Is the amount of arguments from terminal valid? At least is 3 and at most is 7.
+    if (argc < 3 || argc > 7) {
         throwOutOfRangeArguments();
     }
 
@@ -104,6 +101,17 @@ void checkTerminalInputs(int argc, char *argv[], int *isCaseInsensitive, int *is
     fclose(fptr);
 
     // Are there any other optional flags used?
+    checkFlag(argc, argv, isCaseInsensitive, isLineNumber, isWriteToFile);
+}
+
+void checkFile(FILE *fptr, const char *filename) {
+    if (fptr == NULL) {
+        printf("Error: Failed to open %s\n", filename);
+        exit(1);
+    }
+}
+
+void checkFlag(int argc, char *argv[], int *isCaseInsensitive, int *isLineNumber, int *isWriteToFile) {
     switch (argc) {
         case 4:
             if ((argv[3])[1] == 'i') {
@@ -129,13 +137,48 @@ void checkTerminalInputs(int argc, char *argv[], int *isCaseInsensitive, int *is
                 throwInvalidFlag();
             }
             break;
-    }
-}
+        case 6:
+            if ((argv[3])[1] == 'i' && (argv[4])[1] == 'n' && (argv[5])[0] == '<') {
+                *isCaseInsensitive = 1;
+                *isLineNumber = 1;
 
-void checkFile(FILE *fptr, const char *filename) {
-    if (fptr == NULL) {
-        printf("Error: Failed to open %s\n", filename);
-        exit(1);
+                throwInvalidOutputFileName();
+            }
+            else if ((argv[3])[1] == 'n' && (argv[4])[1] == 'i' && (argv[5])[0] == '<') {
+                *isCaseInsensitive = 1;
+                *isLineNumber = 1;
+
+                throwInvalidOutputFileName();
+            }
+            else {
+                throwInvalidFlag();
+            }
+            break;
+        case 7:
+            if ((argv[3])[1] == 'i' && (argv[4])[1] == 'n' && (argv[5])[0] == '<') {
+                *isCaseInsensitive = 1;
+                *isLineNumber = 1;
+
+                const char *fileout = argv[6];
+                FILE *foptr = fopen(fileout, "w");
+                checkFile(foptr, fileout);
+                fclose(foptr);
+                *isWriteToFile = 1;
+            }
+            else if ((argv[3])[1] == 'n' && (argv[4])[1] == 'i' && (argv[5])[0] == '<') {
+                *isCaseInsensitive = 1;
+                *isLineNumber = 1;
+
+                const char *fileout = argv[6];
+                FILE *foptr = fopen(fileout, "w");
+                checkFile(foptr, fileout);
+                fclose(foptr);
+                *isWriteToFile = 1;
+            }
+            else {
+                throwInvalidFlag();
+            }
+            break;
     }
 }
 
@@ -154,25 +197,67 @@ void messageNoPatternFound() {
     exit(1);
 }
 
+void messageToChoice(int isWriteToFile, char stringBuilder[], char *argv[], int isMatched) {
+    if (isWriteToFile == 1) {
+        const char *fileout = argv[6];
+        FILE *foptr = fopen(fileout, "w");
+        checkFile(foptr, fileout);
+
+        char line[MAX_CHARACTERS];
+        while (fgets(line, MAX_CHARACTERS, foptr)) {
+            if (isMatched == 0) {
+                fputs(line, foptr);
+                messageNoPatternFound();
+            }
+            else {
+                fputs(line, foptr);
+            }
+        }
+
+        fclose(foptr);
+    }
+
+    if (isMatched == 0) {
+        messageNoPatternFound();
+    }
+    printf("%s", stringBuilder);
+}
+
 void throwInvalidFlag() {
-    printf("Usage: ./main <pattern> <filename> with optional [-i] for case insensitive search, [-n] for line number\n");
+    printf("Usage: ./main <pattern> <filename> <-i/-n> <-i/-n> [<] <fileout>\n");
+    printf("Usage: Both <-i/-n> are optional, [-i] for case insensitive search and [-n] for line number.\n");
+    printf("Usage: When using \"<\", type in a new file name to write in <fileout>.\n");
     exit(1);
 }
 
 void throwInvalidPattern() {
-    printf("Usage: ./main <pattern> <filename> with optional [-i] for case insensitive search, [-n] for line number\n");
+    printf("Usage: ./main <pattern> <filename> <-i/-n> <-i/-n> [<] <fileout>\n");
+    printf("Usage: Both <-i/-n> are optional, [-i] for case insensitive search and [-n] for line number.\n");
+    printf("Usage: When using \"<\", type in a new file name to write in <fileout>.\n");
     printf("Usage: <pattern> must be a single word you want to find within the desired file.\n");
     exit(1);
 }
 
 void throwInvalidFileName() {
-    printf("Usage: ./main <pattern> <filename> with optional [-i] for case insensitive search, [-n] for line number\n");
+    printf("Usage: ./main <pattern> <filename> <-i/-n> <-i/-n> [<] <fileout>\n");
+    printf("Usage: Both <-i/-n> are optional, [-i] for case insensitive search and [-n] for line number.\n");
+    printf("Usage: When using \"<\", type in a new file name to write in <fileout>.\n");
     printf("Usage: <filename> must have the extension \".txt\" after the file's name.\n");
     exit(1);
 }
 
+void throwInvalidOutputFileName() {
+    printf("Usage: ./main <pattern> <filename> <-i/-n> <-i/-n> [<] <fileout>\n");
+    printf("Usage: Both <-i/-n> are optional, [-i] for case insensitive search and [-n] for line number.\n");
+    printf("Usage: When using \"<\", type in a new file name to write in <fileout>.\n");
+    exit(1);
+}
+
 void throwOutOfRangeArguments() {
-    printf("Usage: ./main <pattern> <filename> with optional [-i] for case insensitive search, [-n] for line number\n");
+    printf("Usage: ./main <pattern> <filename> <-i/-n> <-i/-n> [<] <fileout>\n");
+    printf("Usage: Both <-i/-n> are optional, [-i] for case insensitive search and [-n] for line number.\n");
+    printf("Usage: When using \"<\", type in a new file name to write in <fileout>.\n");
+    printf("Error: Your arguments are out of range.");
     exit(1);
 }
 
