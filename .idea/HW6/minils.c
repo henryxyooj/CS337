43b951c2cc8442d3e2bd9253a138eee61eeac4ec
hhxiong <hhxiong@uwm.edu>
2024-07-15 18:14:27 -0500

    Just got done eating and back from work.  Starting to get a general jist of the assignment.

--- .idea/HW6/minils.c
+++ .idea/HW6/minils.c
@@ -7,6 +7,34 @@ void lsExtended(int argc, char *argv[]){
 
 void checkFlags(int argc, char *argv[]) {
    // check for any flags with a switch statement
-   // a, l, la, none, default = throw error
+   // a, l, la, none; default = throw error
+   int isValidFlag = 0;
 
+   // search through each argv value depending on the argc value
+   for (int i = 0; i < argc; i++) {
+      // check if it has the "-" to denote that it's a flag
+      if (argv[i][0] != '-') {
+         throwInvalidFlag();
+      }
+
+      // then compare the char/string to see if it's actually a valid flag
+      switch ((argv[i])[1]) {
+         case 'a':
+
+            break;
+         case 'l':
+            break;
+         case 'la':
+            break;
+         default:
+            throwInvalidFlag();
+            break;
+      }
+   }
+}
+
+void throwInvalidFlag() {
+    printf("Usage: ./minils <optional command> <optional path>\n");
+    printf("Note: Optional command includes: -l, -a, or -la\nNote: Optional path is your own path.");
+    exit(1);
 }
