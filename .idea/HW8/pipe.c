4ecaddf8e9b6e451ac413801c01040bf73e26da5
hhxiong <hhxiong@uwm.edu>
2024-07-30 23:03:20 -0500

    Final product.

--- .idea/HW8/pipe.c
+++ .idea/HW8/pipe.c
@@ -1,3 +1,28 @@
+/*******************************************************************
+* Author: Henry Xiong
+* Date: July 30, 2024
+
+* Description:
+* This C program implements the following piped unix commands:
+* cat file | wc -l
+* cat file | wc -w
+* cat file | wc -c
+* cat file | sort
+
+Usage:
+* Compile it using a C compiler and run the executable from the command line with appropriate arguments.
+* The format required to input it into the terminal is: [./main] [cat] [filename] [\"|\" or \'|\'] [command: wc or sort] [optional flag for wc: -l or -w or -c]
+
+Citations:
+* https://www.youtube.com/watch?v=S5h3BYg8988&ab_channel=linuxhint
+I had a rough time trying to find ways to open up the file.  This video
+helped me understand it better, but I'm going to review it again this weekend
+along with his code near around the 3:10 mark.  I sort of get that having the buffer for the
+second parameter holds the information of the file, but I understand the first parameter
+which is just the file's name.  By inputting in something like ./mydir/trees.txt, you're giving the
+absolute path to the file, so it makes sense.  Correct me if I'm wrong.
+*******************************************************************/
+
 #include "file.h"
 #include "throws.c"
 
@@ -9,9 +34,13 @@ void checkTerminalPipe(char *argv[]);
 void checkTerminalCommand(int *isWc, int *isSort, char *argv[], int *isNumOfLines, int *isWordCount, int *isNumOfBytes);
 void checkTerminalCommandFlag(char *argv[], int *isNumOfLines, int *isWordCount, int *isNumOfBytes);
 void checkTerminalFilename(char *argv[]);
-
 void checkProcess(pid_t process);
 
+/*******************************************************************
+* Description: creates two processes that directs output from child 1 to child 2
+*               it already creates the beginning portion of the command,
+                so the child 2 process finishes the other half of the full command
+*******************************************************************/
 void pipe_commands(int argc, char *argv[]) {
     int isWc = 0, isSort = 0;
     int isNumOfLines = 0, isWordCount = 0, isNumOfBytes = 0;
@@ -51,6 +80,9 @@ void pipe_commands(int argc, char *argv[]) {
 
         char *c2PidArgs[] = {NULL, NULL, NULL, NULL};
 
+        // if wc is the command being called for, check for which flag is being used and execute it
+        // otherwise check to see if it's sort and execute it
+        // if not either of those, throw an error
         if (isWc == 1) {
             c2PidArgs[0] = "wc";
             c2PidArgs[1] = argv[5];
@@ -72,6 +104,10 @@ void pipe_commands(int argc, char *argv[]) {
     waitpid(c2pid, NULL, 0);
 }
 
+/*******************************************************************
+* Description: Checks each input in the terminal to correctly set up
+                the pipe
+*******************************************************************/
 void checkTerminal(int argc, char *argv[], int *isWc, int *isSort, int *isNumOfLines, int *isWordCount, int *isNumOfBytes) {
     checkTerminalArguments(argc);
     checkTerminalFilename(argv);
@@ -79,6 +115,10 @@ void checkTerminal(int argc, char *argv[], int *isWc, int *isSort, int *isNumOfL
     checkTerminalCommand(isWc, isSort, argv, isNumOfLines, isWordCount, isNumOfBytes);
 }
 
+/*******************************************************************
+* Description: part of the checkTerminalCommand, it checks out to see
+                if argv[5] is a valid flag used for this mini pipe program
+*******************************************************************/
 void checkTerminalCommandFlag(char *argv[], int *isNumOfLines, int *isWordCount, int *isNumOfBytes) {
     char flag[MAX_CHAR];
     strcpy(flag, argv[5]);
@@ -97,6 +137,10 @@ void checkTerminalCommandFlag(char *argv[], int *isNumOfLines, int *isWordCount,
     }
 }
 
+/*******************************************************************
+* Description: Checks to see if the command requested by user is either
+                wc or sort
+*******************************************************************/
 void checkTerminalCommand(int *isWc, int *isSort, char *argv[], int *isNumOfLines, int *isWordCount, int *isNumOfBytes) {
     if (strcmp(argv[4], "wc") == 0) {
         *isWc = 1;
@@ -115,12 +159,20 @@ void checkTerminalCommand(int *isWc, int *isSort, char *argv[], int *isNumOfLine
     }
 }
 
+/*******************************************************************
+* Description: Checks to see if the user correctly inputted the pipe
+                operator into the terminal
+*******************************************************************/
 void checkTerminalPipe(char *argv[]) {
     if ((strcmp(argv[3], "|") != 0) && (strcmp(argv[3], "'|'") != 0)) {
         throwInvalidPipe();
     }
 }
 
+/*******************************************************************
+* Description: Searches for the pattern given from terminal to the requested
+                file
+*******************************************************************/
 void checkTerminalFilename(char *argv[]) {
     char filename[MAX_CHAR];
     strcpy(filename, argv[2]);
@@ -128,17 +180,25 @@ void checkTerminalFilename(char *argv[]) {
     // https://www.youtube.com/watch?v=S5h3BYg8988&ab_channel=linuxhint
     struct stat buffer;
     if (stat(filename, &buffer) != 0) {
-        puts("Error occurred\n");
+        puts("Error occurred opening the %s\n", filename);
         exit(1);
     }
 }
 
+/*******************************************************************
+* Description: Checks to see if the amount of arguments inputted by the
+                user is valid
+*******************************************************************/
 void checkTerminalArguments(int argc) {
     if (argc > 6 || argc < 5) {
         throwOutOfBoundsTerminalInputs();
     }
 }
 
+/*******************************************************************
+* Description: checks to see if there's anything wrong when creating
+                a new process
+*******************************************************************/
 void checkProcess(pid_t process) {
     if (process == -1) {
         throwProcessError();
--- .idea/HW8/throws.c
+++ .idea/HW8/throws.c
@@ -1,3 +1,14 @@
+/*******************************************************************
+* Author: Henry Xiong
+* Date: July 30, 2024
+
+* Description:
+* This C program contains all of the error handling for the pipe.c program
+
+Usage:
+* include this file with pipe.c to make sure any errors that may occur in pipe.c will throw errors to the user when inputting
+* any wrong input or if any thing at all
+*******************************************************************/
 void throwOutOfBoundsTerminalInputs();
 void throwInvalidCommand();
 void throwInvalidPipe();
@@ -7,45 +18,74 @@ void throwProcessError();
 void throwArgError();
 void throwExecvpError();
 
+/*******************************************************************
+* Description: if the user inputted too many arguments to the terminal
+*******************************************************************/
 void throwOutOfBoundsTerminalInputs() {
     puts("Error: Invalid amount of arguments to the terminal.");
     puts("Usage: [./main] [cat] [filename] [\"|\" or \'|\'] [command: wc or sort] [optional flag for wc: -l or -w or -c]");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error if a user doesn't input any of the two
+                commands: sort or wc
+*******************************************************************/
 void throwInvalidCommand() {
     puts("Error: pipe.c currently supports only these two commands: \"wc\" or \"sort\"");
     puts("Usage: [./main] [cat] [filename] [\"|\" or \'|\'] [command: wc or sort] [optional flag for wc: -l or -w or -c]");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error if the user doesn't input the pipe operator
+                correctly to the terminal
+*******************************************************************/
 void throwInvalidPipe() {
     puts("Error: Use the pipe command with either: \"|\" or \'|\'");
     puts("Usage: [./main] [cat] [filename] [\"|\" or \'|\'] [command: wc or sort] [optional flag for wc: -l or -w or -c]");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error if a user doesn't input any of the supported
+                flags: -l, -w, -c
+*******************************************************************/
 void throwInvalidFlag() {
     puts("Error: pipe.c currently supports only these flags for wc command: [-l], [-w], or [-c]");
     puts("Usage: [./main] [cat] [filename] [\"|\" or \'|\'] [command: wc or sort] [optional flag for wc: -l or -w or -c]");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error if there's a problem when creating a pipe
+*******************************************************************/
 void throwPipeError() {
     puts("Error piping.");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error if there's a problem when creating a new process
+*******************************************************************/
 void throwProcessError() {
     puts("Error creating child process.");
     exit(1);
 }
 
+/*******************************************************************
+* Description: shouldn't happen at all with all of the safety nets before this,
+                but before the execvp function, if there's any problem at all, throw this error
+*******************************************************************/
 void throwArgError() {
     puts("Error building arguments for cat command.");
     exit(1);
 }
 
+/*******************************************************************
+* Description: throws an error after the execvp function if for some reason
+                it's still going
+*******************************************************************/
 void throwExecvpError() {
     puts("Error with execvp function.");
     exit(1);
