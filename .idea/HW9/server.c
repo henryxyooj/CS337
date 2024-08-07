6e1a6a96ae7f3ea5f6042fe6cff581b3a6e9c3c2
hhxiong <hhxiong@uwm.edu>
2024-08-06 21:23:14 -0500

    I'm going to go through some of the powerpoint slides to see if I could do any error handling

--- .idea/HW9/server.c
+++ .idea/HW9/server.c
@@ -1,3 +1,15 @@
+/*******************************************************************
+* Author: Henry Xiong
+* Date: August 6, 2024
+
+* Description: 
+* This C program is used to communicate with the client by supplying them with a key to their message.
+* It then decrypts their message through the Caesar Cipher logic
+
+Usage:
+* The maximum amount of clients is 5, use the compiler and type in the command ./server to use this program
+*******************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,16 +22,26 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+/*******************************************************************
+* Description: this function decrypts the message received from the client
+                that's been shifted by a randomly generated key 
+*******************************************************************/
 void decrypt(char message[BUFFER_SIZE], int key) {
     for (unsigned long i = 0; i < strlen(message); i++) {
         if ((message[i] >= 'a' && message[i] <= 'z')) {
             message[i] = ((message[i] - 'a') - key) % 26 + 'a';
-        } else if ((input[i] >= 'A' && input[i] <= 'Z')) {
+        } else if ((message[i] >= 'A' && message[i] <= 'Z')) {
             message[i] = ((message[i] - 'A') - key) % 26 + 'A';
         }
     }
 }
 
+/*******************************************************************
+* Description: hosts a server that allows communication with the client that allows
+                them to enter a message that gets encoded and then decoded into the
+                the server.  The encryption has a randomly generated key from 1 to
+                25 that shifts the ASCII value of each character in the message
+*******************************************************************/
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -43,8 +65,8 @@ int main() {
         return 1;
     }
 
-    // Listen for connections.
-    if (listen(server_fd, 1) < 0) {
+    // Listen for connections. 
+    if (listen(server_fd, 1) < 0) {  
         perror("listen");
         return 1;
     }
@@ -56,7 +78,7 @@ int main() {
             return 1;
         }
 
-        // update client count
+        // update client count 
         client_count++;
         printf("Connecting to Client %d\n", client_count);
         if (client_count > 5) {
@@ -64,17 +86,19 @@ int main() {
             close(new_socket);
             return 1;
         }
+        
+        pid_t pid = fork();
+        if (pid == 0) {  // child process
+            close(server_fd);
 
-        if (fork() == 0) {  // child process
-            // what was sent from the client?
-            char msg_from_client[BUFFER_SIZE] = {0};
-            read(new_socket, msg_from_client, BUFFER_SIZE);
-            printf("%s", msg_from_client);
+            // what was sent from the client? "Hello from client, can you send me the key"
+            char greeting_from_client[BUFFER_SIZE] = {0};
+            read(new_socket, greeting_from_client, BUFFER_SIZE);
+            printf("%s", greeting_from_client);
 
-            // generate a random key
+            // generate a random key 
             srand(time(NULL));
             int key = rand() % (25 - 1 + 1) + 1;
-            printf("Key from server: %d", key);
 
             // convert the key from integer to string with sprintf and send it to client
             char key_to_client[BUFFER_SIZE] = {0};
@@ -83,28 +107,28 @@ int main() {
 
             while (1) {
                 // reads the message that's going to be converted
-                read(new_socket, key_to_client, BUFFER_SIZE);
+                char msg_from_client[BUFFER_SIZE] = {0};
+                read(new_socket, msg_from_client, BUFFER_SIZE);
 
-                if (strcmp(key_to_client, "quit") == 0 || strcmp(key_to_client, "QUIT") == 0) {
+                // once the user inputs quit into the terminal, terminate the loop
+                if (strcmp(msg_from_client, "quit") == 0 || strcmp(msg_from_client, "QUIT") == 0) {
                     printf("Client %d terminated\n", client_count);
                     break;
                 }
+                
+                decrypt(msg_from_client, key);
 
-                char encoded_msg[BUFFER_SIZE] = {0};
-                decrypt(encoded_msg, key);
-                printf("Encoded message: %s");
-
-                write(new_socket, key_to_client, strlen(key_to_client));
+                write(new_socket, msg_from_client, strlen(msg_from_client));
             }
 
             close(new_socket);
+            exit(1);
         }
         else {  // parent process
             close(new_socket);
         }
     }
 
+    close(server_fd);
     return 0;
 }
-
-
